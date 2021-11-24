12addbad06711200c8f060427b712c322c0564d1
sangeetha <sangeetha@apples-MacBook-Pro.local>
2021-11-24 21:11:22 +0530

    changes in easyshare

--- tests/firsttest.js
+++ tests/firsttest.js
@@ -1,4 +1,6 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
+var clickHover= Selector(()=> document.querySelector('service-wrapper').shadowRoot.querySelector('.meeting-card-wrapper'));
+var hoverClick= Selector(() => document.querySelector("body > service-wrapper").shadowRoot.querySelector("div.container > div:nth-child(1) > div:nth-child(3) > div > div.meeting-top > div.list-popover > ul > li:nth-child(2)"));
 
 fixture `Getting Started`// declare the fixture
 .page `https://easysharestage.setmore.com/home/`;
@@ -26,14 +28,27 @@ test('My first test', async test => {
 
             .expect(sel.exists).ok;
 
-       const hoverService= await Selector(()=>document.querySelector('#bookinglinkShare  > service-wrapper').shadowRoot.querySelector('.meeting-card-wrapper').hover);
+    //    const hoverService= await Selector(()=>document.querySelector('#bookinglinkShare  > service-wrapper').shadowRoot.querySelector('.meeting-card-wrapper').hover());
+       await test 
+       .hover(clickHover)
+    //    .click(hoverClick)
+    //    .hover(document.querySelector('#bookinglinkShare  > service-wrapper').shadowRoot.querySelector('.meeting-card-wrapper'))
        await test.debug()
 
-        var doExists=await Selector(()=>document.querySelector("#bookinglinkShare > service-wrapper").shadowRoot.querySelector("div > div:nth-child(1) > div.card-section > div:nth-child(2) > div.meeting-top > button").exists);
-        if(doExists){
-            console.log("trace");
-        }
+
+        // var doExists=await Selector(()=>document.querySelector("#bookinglinkShare > service-wrapper").shadowRoot.querySelector("div > div:nth-child(1) > div.card-section > div:nth-child(2) > div.meeting-top > button").exists);
+        // if(doExists){
+        //     console.log("trace");
+        // }
                
          
             
     });
+
+    // Run in iphone:
+    // testcafe "chrome:emulation:device=iphone X" tests/sample-fixture.js
+
+
+    // testcafe chrome ./tests/*.js --reporter allure
+
+    // allure generate allure/allure-results --clean -o allure/allure-report && allure open allure/allure-report
